eaefe7af2ae835cbce75d994f3ad159ba60e5150
qqqestion <toronto.city.nick@gmail.com>
2020-09-20 21:20:17 +0300

    Init Android Project

---
+++ app/src/main/java/com/emoji/eatogether/MainActivity.java
@@ -0,0 +1,14 @@
+package com.emoji.eatogether;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
