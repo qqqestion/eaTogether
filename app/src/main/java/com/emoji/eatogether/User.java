dcf26fc374cb3b551771b381176f5a0632b2f335
qqqestion <toronto.city.nick@gmail.com>
2020-09-27 23:16:28 +0300

    Регистраций и вход сделаны. В профиле идет отображение всех полей пользователя

---
+++ app/src/main/java/com/emoji/eatogether/User.java
@@ -0,0 +1,24 @@
+package com.emoji.eatogether;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+
+    public String name;
+    public String surname;
+    public String birthday;
+    public String description;
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String name, String surname, String birthday, String description) {
+        this.name = name;
+        this.surname = surname;
+        this.birthday = birthday;
+        this.description = description;
+    }
+}
+
