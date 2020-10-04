b17bb0fd3ca1eeec68b2c92150f595cc6d30f488
qqqestion <toronto.city.nick@gmail.com>
2020-10-04 13:24:42 +0300

    Исправлены андроид конфликты после слияния

--- app/src/main/java/com/emoji/eatogether/ProfileActivity.java
+++ app/src/main/java/com/emoji/eatogether/ProfileActivity.java
@@ -25,33 +25,33 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         mAuth = FirebaseAuth.getInstance();
-        final TextView emailTextView = findViewById(R.id.email);
-        final TextView nameTextView = findViewById(R.id.name);
-        final TextView surnameTextView = findViewById(R.id.surname);
-        final TextView birthdayTextView = findViewById(R.id.birthday);
-        final TextView descriptionTextView = findViewById(R.id.description);
-
-        FirebaseUser user = mAuth.getCurrentUser();
-        if (user == null) {
-            Intent intent = new Intent(getApplication(), LoginActivity.class);
-            startActivity(intent);
-        }
-        emailTextView.setText(user.getEmail());
-        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("users").child(user.getUid());
-        userRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User userData = snapshot.getValue(User.class);
-                nameTextView.setText(userData.name);
-                surnameTextView.setText(userData.surname);
-                birthdayTextView.setText(userData.birthday);
-                descriptionTextView.setText(userData.description);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
+//        final TextView emailTextView = findViewById(R.id.email);
+//        final TextView nameTextView = findViewById(R.id.name);
+//        final TextView surnameTextView = findViewById(R.id.surname);
+//        final TextView birthdayTextView = findViewById(R.id.birthday);
+//        final TextView descriptionTextView = findViewById(R.id.description);
+//
+//        FirebaseUser user = mAuth.getCurrentUser();
+//        if (user == null) {
+//            Intent intent = new Intent(getApplication(), LoginActivity.class);
+//            startActivity(intent);
+//        }
+//        emailTextView.setText(user.getEmail());
+//        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("users").child(user.getUid());
+//        userRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                User userData = snapshot.getValue(User.class);
+//                nameTextView.setText(userData.name);
+//                surnameTextView.setText(userData.surname);
+//                birthdayTextView.setText(userData.birthday);
+//                descriptionTextView.setText(userData.description);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
     }
 }
