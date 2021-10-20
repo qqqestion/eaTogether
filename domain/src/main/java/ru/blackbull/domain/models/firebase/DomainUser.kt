9d46a1627ef02df4b890946b4b356ac7589dc8a1
qqqestion <toronto.city.nick@gmail.com>
2021-10-20 07:59:16 +0300

    fix: delete useless property phone in DomainUser.kt

--- domain/src/main/java/ru/blackbull/domain/models/firebase/DomainUser.kt
+++ domain/src/main/java/ru/blackbull/domain/models/firebase/DomainUser.kt
@@ -3,19 +3,18 @@ package ru.blackbull.domain.models.firebase
 import ru.blackbull.domain.models.Location
 
 data class DomainUser(
-    var id: String? = null ,
-    var phone: String? = null ,
-    var firstName: String? = null ,
-    var lastName: String? = null ,
-    var description: String? = null ,
-    var birthday: Long? = null ,
-    var isRegistrationComplete: Boolean = true ,
+    val id: String? = null ,
+    val firstName: String? = null ,
+    val lastName: String? = null ,
+    val description: String? = null ,
+    val birthday: Long? = null ,
+    val isRegistrationComplete: Boolean = true ,
     var mainImageUri: String? = null ,
     var images: List<String> = listOf() ,
-    var likedUsers: List<String> = mutableListOf() ,
-    var dislikedUsers: List<String> = mutableListOf() ,
+    val likedUsers: List<String> = mutableListOf() ,
+    val dislikedUsers: List<String> = mutableListOf() ,
     var friendList: List<String> = mutableListOf() ,
-    var lastLocation: Location? = null
+    val lastLocation: Location? = null
 )
 
 enum class FriendState {
